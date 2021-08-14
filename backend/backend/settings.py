620552ea3ca8a4e3f08f6f49a2b55e66c1c498ba
Ferhat Sevim <ofk16fsm@gmail.com>
2021-08-14 20:03:09 +0200

    Added web application files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,7 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # We whitelist localhost:3000 because that's where frontend will be served
 CORS_ORIGIN_WHITELIST = [
